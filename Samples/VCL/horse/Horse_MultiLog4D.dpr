5e0b7acdc4cd33463af00e57f80390d116c2fcb8
Alvin Almonte <34565384+AlvinAlmonte20@users.noreply.github.com>
2025-05-23 01:45:13 -0400

    Now it works using MultiLog4D.Config unit
    Sample modified to make it works using MultiLog4D.Config unit.

--- Samples/VCL/horse/Horse_MultiLog4D.dpr
+++ Samples/VCL/horse/Horse_MultiLog4D.dpr
@@ -9,6 +9,7 @@ uses
   MultiLog4D.Common,
   MultiLog4D.Util,
   MultiLog4D.Types,
+  MultiLog4D.Config,
   System.IOUtils,
   System.SysUtils;
 
@@ -18,26 +19,17 @@ begin
   LOutputLogPath := TPath.Combine(ExtractFilePath(ParamStr(0)), 'MyLog');
   ForceDirectories(LOutputLogPath);
 
-  TMultiLog4DUtil
-    .Logger
-    .Tag('MultiLog4D')
-    .Output([loFile, loConsole])
-    .FileName(TPath.Combine(LOutputLogPath, 'Log.txt'))
-    .LogWriteInformation('>>>>>>>>>> Starting <<<<<<<<<<');
+  MultiLog4D.Config.Log.LogWriteInformation('>>>>>>>>>> Starting <<<<<<<<<<')
 
   THorse
     .Get('/test1',
     procedure(Req: THorseRequest; Res: THorseResponse)
     begin
-      TMultiLog4DUtil
-        .Logger
-        .LogWriteInformation('Before Test1');
+      MultiLog4D.Config.Log.LogWriteInformation('Before Test1');
 
       Res.Send('test1');
 
-      TMultiLog4DUtil
-        .Logger
-        .LogWriteInformation('After Test1');
+      MultiLog4D.Config.Log.LogWriteInformation('After Test1');
     end
     )
     .Get('/test2',
@@ -45,9 +37,7 @@ begin
     begin
       Res.Send('test2');
 
-      TMultiLog4DUtil
-        .Logger
-        .LogWriteInformation('Test2');
+      MultiLog4D.Config.Log.LogWriteInformation('Test2');
     end
     )
     .Get('/test3',
@@ -55,9 +45,7 @@ begin
     begin
       Res.Send('test3');
 
-      TMultiLog4DUtil
-        .Logger
-        .LogWriteInformation('Test3');
+      MultiLog4D.Config.Log.LogWriteInformation('Test3');
     end
     );
 
